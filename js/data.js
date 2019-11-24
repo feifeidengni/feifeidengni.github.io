dc283f6433492fed6f095710ffcbdecea22494f5
feifeidengni <1790917605@qq.com>
2019-11-24 23:45:22 +0800

    项目进展

---
+++ js/data.js
@@ -0,0 +1,29 @@
+/**
+ * type:
+ *   DIV  JS  H5 CSS 3
+ * **/
+var flyData = [
+    {
+      type: "Css3",
+      author: '我爱你',
+      title: '我感觉你在为难我',
+      desc: '这个案例很屌',
+      time: '2019-09-26',
+      src: '3D Drag'
+    }, {
+      type: "Js",
+      author: '飞飞',
+      title: '测试一下',
+      desc: '这个案例很屌',
+      time: '2019-09-27',
+      src: 'images'
+    },{
+        type: "sql",
+        author: '小姐姐',
+        title: '测试一下',
+        desc: '完美的爱情',
+        time: '2019-09-27',
+        src: 'love'
+    }
+  ]
+  
